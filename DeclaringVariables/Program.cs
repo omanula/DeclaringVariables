ccb197d54653f30feae8985c9b48e6e2e8b15196
Olivia <31357493+omanula@users.noreply.github.com>
2017-08-29 19:47:31 -0600

    DeclaringVariables (Part 1)

---
+++ DeclaringVariables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DeclaringVariables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Don't forget the semi-colon at the end of a line!!! //
+
+            int x;
+            int y;
+            /* Declaration of variables. Allocate space in the memory  large
+             * enough to store numeric values. Variables able to hold values. 
+             * Values (integers) can be applied to the variables now. 
+             * int = integer in C#, no fractions/decimals (2,147,000 max)
+             */
+
+            x = 7;
+            // Assign value to variable //
+            y = x + 3;
+            Console.WriteLine(y);
+            // Capital W in Write and capital L in Line needed//
+
+            Console.ReadLine();
+            // Capital R in Read and capital L in Line needed//
+
+        }
+    }
+}
