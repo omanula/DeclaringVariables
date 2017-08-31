c540330200236d246a23158a6e3e57f9f7205d0e
Olivia <31357493+omanula@users.noreply.github.com>
2017-08-30 21:38:40 -0600

    Declaring Variables (Part 2)

--- DeclaringVariables/Program.cs
+++ DeclaringVariables/Program.cs
@@ -6,8 +6,9 @@ namespace DeclaringVariables
     {
         public static void Main(string[] args)
         {
-            // Don't forget the semi-colon at the end of a line!!! //
+            // Don't forget the semi-colon at the end of a line!!! 
 
+            /*
             int x;
             int y;
             /* Declaration of variables. Allocate space in the memory  large
@@ -15,15 +16,53 @@ namespace DeclaringVariables
              * Values (integers) can be applied to the variables now. 
              * int = integer in C#, no fractions/decimals (2,147,000 max)
              */
-
+            /*
             x = 7;
-            // Assign value to variable //
+            // Assign value to variable 
             y = x + 3;
             Console.WriteLine(y);
-            // Capital W in Write and capital L in Line needed//
+            // Capital W in Write and capital L in Line needed
+            */
+
+
+            //string myFirstName;
+            // Camel Case = capitals at each new word 
+            //myFirstName = "Olivia";
+
+            //string myFirstName = "Olivia";
+
+            //var myFirstName = "Olivia";
+            //var myFirstName = "";
+
+            // string myfirstname;
+
+            //Console.WriteLine(myFirstName);
+            // C# IS case sensitive 
+
+
+
+
+            int x = 7;
+            string y = "Olivia";
+            string myFirstTry = x.ToString() + y;
+            // How can int 7 be treated as a string to combine with another?
+            // Plus sign is a "string concatination operator" (concatinate or add depending on situation)
+
+            Console.WriteLine(myFirstTry);
+
+
+
+
+
 
             Console.ReadLine();
-            // Capital R in Read and capital L in Line needed//
+            // Capital R in Read and capital L in Line needed
+
+
+
+
+
+            
 
         }
     }
